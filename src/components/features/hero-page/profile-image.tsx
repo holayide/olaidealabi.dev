c481ad018597345a4b99342f0c492e7ae731a378
olaide <holaryidealabi@gmail.comgit config --global user.email holaryidealabi@gmail.com>
2026-01-05 00:35:53 -0800

    adjusting blur

--- src/components/features/hero-page/profile-image.tsx
+++ src/components/features/hero-page/profile-image.tsx
@@ -4,7 +4,10 @@ export default function ProfileImage() {
   return (
     <div className="order-1 992:order-2 flex justify-center">
       <div className="relative w-70 h-70 md:w-80 md:h-80 lg:w-90 lg:h-90">
-        <div className="absolute inset-0 rounded-full bg-linear-to-tr from-accent to-accent-soft blur-[25px] sm:blur-2xl lg:blur-[60px] opacity-20 animate-glow" />
+        <div
+          style={{ filter: "blur(60px)" }}
+          className="absolute inset-0 rounded-full bg-linear-to-tr from-accent to-accent-soft blur-[30px] sm:blur-[45px] lg:blur-[60px] opacity-20 animate-glow"
+        />
 
         <div className="h-full relative rounded-full overflow-hidden bg-light-grey border-4 border-accent/30 shadow-2xl ring-8 ring-accent/10">
           <img
